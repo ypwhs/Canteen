335cafb582644dc0ccaef2cdff960f9d3986800d
ypwhs <915505626@qq.com>
2015-04-14 21:19:32 +0800

    master

---
+++ Canteen/src/com/bymxd/canteen/Reg.java
@@ -0,0 +1,14 @@
+package com.bymxd.canteen;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Reg extends Activity {
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reg);
+
+    }
+	
+}
