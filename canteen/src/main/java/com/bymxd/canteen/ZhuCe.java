366a4b796662c21675189f10bb789aebeb66e67a
ypwhs <915505626@qq.com>
2015-04-16 23:36:51 +0800

    master
    迁移至android studio

---
+++ canteen/src/main/java/com/bymxd/canteen/ZhuCe.java
@@ -0,0 +1,14 @@
+package com.bymxd.canteen;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ZhuCe extends Activity {
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_denglu);
+
+    }
+	
+}
